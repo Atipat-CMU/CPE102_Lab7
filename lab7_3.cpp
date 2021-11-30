0831c69258b45d83f7bc29bbbc0ecf330dd26f7f
AtipatD <atipat_d@cmu.ac.th>
2021-11-30 15:55:10 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,38 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+  int result;
+  a %= 360;
+  b %= 360;
+
+  if(a < 0){
+    a = 360 + a;
+  }
+  if(b < 0){
+    b = 360 + b;
+  }
+
+  result = a-b;
+  if(result < 0){
+    result = -result;
+  }
+  
+  if(result > 180){
+    result = 360 - result;
+  }
+
+  return b;
+}
 
 int main(){
   cout << adiff(180,270);
+  /*cout << adiff(180,270);
   cout << adiff(210,45);
   cout << adiff(0,360);
   cout << adiff(10,350);
   cout << adiff(95,260);
   cout << adiff(90,-90);
   cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(60,244);*/
 }
